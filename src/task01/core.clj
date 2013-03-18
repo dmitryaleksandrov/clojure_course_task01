4129c87bc1379ba633dc9c797a25c6e1de05d652
dima <dima@laptop.(none)>
2013-03-19 00:44:51 +0400

    first homework

--- src/task01/core.clj
+++ src/task01/core.clj
@@ -2,6 +2,13 @@
   (:require [pl.danieljanus.tagsoup :refer :all])
   (:gen-class))
 
+(defn visit [links [tag attrs & chlds]]
+  (defn find-links [elements]
+    (map #(:href (nth % 1)) (filter #(= (nth % 0) :a) elements)))
+
+  (if (= (:class attrs) "r")
+    (concat links (find-links chlds))
+    (reduce visit links (filter coll? chlds))))
 
 (defn get-links []
 " 1) Find all elements containing {:class \"r\"}.
@@ -19,11 +26,9 @@ The link from the example above is 'https://github.com/clojure/clojure'.
   3) Return vector of all 10 links.
 
 Example: ['https://github.com/clojure/clojure', 'http://clojure.com/', . . .]
-"
+"  
   (let [data (parse "clojure_google.html")]
-    nil))
+    (visit [] data)))
 
 (defn -main []
   (println (str "Found " (count (get-links)) " links!")))
-
-
